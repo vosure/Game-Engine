a15bbc670518b1dae3cfde4391064024e864f856
vosure <vosure1@gmail.com>
2019-03-09 22:38:01 +0300

    logging system

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-
 #include "Engine/Application.h"
-
+#include "Engine/Log.h"
 #include "Engine/EntryPoint.h"
---
+++ Engine/src/Engine/Log.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Engine {
+
+	class ENGINE_API Log
+	{
+	public:
+
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+
+
+	};
+
+}
+
+#define ENGINE_LOG_ERROR(...) ::Engine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ENGINE_LOG_WARN(...) ::Engine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ENGINE_LOG_INFO(...) ::Engine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ENGINE_LOG_TRACE(...) ::Engine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ENGINE_LOG_FATAL(...) ::Engine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+#define CLIENT_LOG_ERROR(...) ::Engine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CLIENT_LOG_WARN(...) ::Engine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CLIENT_LOG_INFO(...) ::Engine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CLIENT_LOG_TRACE(...) ::Engine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CLIENT_LOG_FATAL(...) ::Engine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
